dcaba6fee8bf9a1c705e872fba0a2451f5a73149
Roland Grunberg <rgrunber@redhat.com>
2020-10-14 15:34:06 -0400

    Implement methods in newly created type from an inherited sealed class.
    - When a non-existent type is permitted by a sealed class, the newly
      created type should implement the necessary methods
    - Related #1553
    
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corrections/proposals/NewCUProposal.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/corrections/proposals/NewCUProposal.java
@@ -48,6 +48,10 @@ import org.eclipse.jdt.internal.core.manipulation.StubUtility;
 import org.eclipse.jdt.internal.core.manipulation.util.BasicElementLabels;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
 import org.eclipse.jdt.internal.corext.dom.IASTSharedValues;
+import org.eclipse.jdt.internal.corext.fix.AddUnimplementedMethodsOperation;
+import org.eclipse.jdt.internal.corext.fix.CompilationUnitRewriteOperationsFixCore.CompilationUnitRewriteOperation;
+import org.eclipse.jdt.internal.corext.fix.IProposableFix;
+import org.eclipse.jdt.internal.corext.fix.UnimplementedCodeFixCore;
 import org.eclipse.jdt.internal.corext.util.JavaModelUtil;
 import org.eclipse.jdt.internal.corext.util.Messages;
 import org.eclipse.jdt.ls.core.internal.corext.refactoring.nls.changes.CreateFileChange;
@@ -276,6 +280,7 @@ public class NewCUProposal extends ChangeCorrectionProposal {
 			CompositeChange change = new CompositeChange(fName);
 			change.add(new CreateFileChange(targetType.getResource().getRawLocation(), "", ""));
 			change.add(constructNewCUChange(parentCU));
+
 			return change;
 		} else {
 			return null;
@@ -353,6 +358,25 @@ public class NewCUProposal extends ChangeCorrectionProposal {
 		String lineDelimiter = StubUtility.getLineDelimiterUsed(fCompilationUnit.getJavaProject());
 		String typeStub = constructTypeStub(cu, fTypeNameWithParameters, Flags.AccPublic, lineDelimiter);
 		String cuContent = constructCUContent(cu, typeStub, lineDelimiter);
+
+		IType cuType = fCompilationUnit.findPrimaryType();
+		String[] permittedNames = cuType.getPermittedSubtypeNames();
+		boolean isPermitted = Arrays.asList(permittedNames).stream().anyMatch(p -> fTypeNameWithParameters.equals(p));
+
+		if (isPermitted) {
+			cu.createType(typeStub, null, false, null);
+			ASTParser parser = ASTParser.newParser(IASTSharedValues.SHARED_AST_LEVEL);
+			parser.setSource(cu);
+			parser.setResolveBindings(true);
+			CompilationUnit cuRoot = (CompilationUnit) parser.createAST(null);
+
+			AddUnimplementedMethodsOperation operation = new AddUnimplementedMethodsOperation((ASTNode) cuRoot.types().get(0));
+			if (operation.getMethodsToImplement().length > 0) {
+				IProposableFix fix = new UnimplementedCodeFixCore(CorrectionMessages.UnimplementedMethodsCorrectionProposal_description, cuRoot, new CompilationUnitRewriteOperation[] { operation });
+				return fix.createChange(null);
+			}
+		}
+
 		CompilationUnitChange cuChange = new CompilationUnitChange("", cu);
 		cuChange.setEdit(new InsertEdit(0, cuContent));
 		return cuChange;
